ac5d176299a14fd8e73eea9b92ba6625ad65c027
mambax7 <mambax7@gmail.com>
2021-02-05 05:42:37 -0500

    english

---
+++ language/english/modinfo.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Jill Query module
+ *
+ * You may not change or alter any portion of this comment or credits
+ * of supporting developers from this source code or any supporting source code
+ * which is considered copyrighted (c) material of the original comment or credit authors.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * @copyright  The XOOPS Project http://sourceforge.net/projects/xoops/
+ * @license    http://www.fsf.org/copyleft/gpl.html GNU public license
+ * @package    Jill Query
+ * @since      2.5
+ * @author     jill lee(tnjaile@gmail.com)
+ * @version    $Id $
+ **/
+
+xoops_loadLanguage('modinfo_common', 'tadtools');
+
+define('_MI_JILLQUERY_ADMENU3','Tag Management');
+define('_MI_JILLQUERY_ADMENU3_DESC','Make classification labels for query data.');
+define('_MI_JILLQUERY_NAME','Simple query system');
+define('_MI_JILLQUERY_AUTHOR','tnjaile@gmail.com');
+define('_MI_JILLQUERY_CREDITS','tad');
+define('_MI_JILLQUERY_DESC','This module is for managers to import EXCEL files, and general users can query personal records by filling in data. Uses such as: results query, admission list, etc.');
+define('_MI_JILLQUERY_AUTHOR_WEB','https://github.com/tnjaile/jill_query');
+define('_MI_JILLQUERY_ADMENU1','Data Management');
+define('_MI_JILLQUERY_ADMENU1_DESC','Data Management');
+
+define('_MI_JILL_QUERY_SHOW_BLOCK_NAME','Projects currently available for query');
+define('_MI_JILL_QUERY_SHOW_BLOCK_DESC','Currently queryable project block (jill_query_show)');
+
+define('_MI_JILLQUERY_ADMENU2','Import and create');
+define('_MI_JILLQUERY_ADMENU2_DESC','Import Excel directly to create query data.');
+
+define('_MI_JILL_QUERY_PUBLIC_BLOCK_NAME','Public data block');
+define('_MI_JILL_QUERY_PUBLIC_BLOCK_DESC','Public data block block (jill_query_public)');
